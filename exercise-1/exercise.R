270c61cc525f152a08cf27a1bab38e4292ef4bad
aksor7 <aksor7@gmail.com>
2017-10-17 20:17:08 -0700

    fixed code in proper exercise file

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,22 +1,22 @@
 # Exercise 1: Creating and Indexing Vectors
 
 # Create a vector `first.ten` that has the values 10 through 20 in it (using the : operator)
-
+first.ten <- 10:20 
 
 # Create a vector `next.ten` that has the values 21 through 30 in it (using the seq operator)
-
+next.ten <- seq(21:30)
 
 # Create a vector `all.numbers` by combining the vectors `first.ten` and `next.ten`
-
+all.numbers <- c(first.ten,next.ten)
 
 # Create a variable `eleventh` that is equal to the 11th element in your vector `all.numbers`
-
+elventh <- all.numbers[11]
 
 # Create a vector `some.numbers` that is equal to the second through the 5th elements of `all.numbers`
-
+some.numbers <- all.numbers[2:5]
 
 # Create a variable `len` that is equal to the length of your vector `all.numbers`
-
+odd <- seq(1, 100, 2)
 
 ### Bonus ###
 
@@ -25,11 +25,3 @@
 
 # Using the `all` and `%%` operators, confirm that all of the numbers in your `odd` vector are odd
 
-first.ten <- 10:20
-next.ten <- seq(21:30)
-all.numbers <- c(first.ten, next.ten)
-eleventh <- all.numbers[11]
-some.numbers <- all.numbers[2:5]
-len <- length(all.numbers)
-odd <- seq(1, 100, 2)
-test <- all(odd %% 2 == 1)
